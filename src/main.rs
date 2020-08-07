6155f674d6226b4ab478e11f4fd2ec3e4afbbacb
Michael Graham <michael@forested.earth>
2020-08-06 20:57:18 -0700

    Initial mirror image writing implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate image;
+
+use std::{
+    io::{self, Write},
+};
+
+use image::{GenericImageView, imageops};
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Cli {
+    path: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    let mut stdout = io::stdout();
+    let img = image::open(&args.path).unwrap();
+    let mir = imageops::flip_horizontal(&img);
+    let (width, height) = img.dimensions();
+    let dwidth = 2 * width as u32;
+    let mut combined_img = image::ImageBuffer::new(dwidth, height);
+
+    for (x, y, pixel) in combined_img.enumerate_pixels_mut() {
+        if x < width {
+            *pixel = *mir.get_pixel(x, y);
+        } else {
+            let nx = x - width;
+            *pixel = img.get_pixel(nx, y);
+        }
+    }
+
+    writeln!(stdout, "{}, {}", "got path: ", &args.path).unwrap();
+    writeln!(stdout, "combined output dimensions {:?}", combined_img.dimensions()).unwrap();
+    combined_img.save("fixtures/test.png").unwrap();
 }
